6ab1e0dcaa36857624af82f050b762c4d4aa3ca4
Sergei Sapoznikov <sergei.sapoznikov@gmail.com>
2015-11-29 19:14:02 +0000

    Clean up Update Planner logic

--- src/reducers/planner.js
+++ src/reducers/planner.js
@@ -1,4 +1,4 @@
-import { UPDATE_PLANNER } from '../actions/planner';
+import { UPDATE_PLANNER, LOAD_DEFAULT } from '../actions/planner';
 import VariableStarPlanner from '../utils/variableStarPlanner';
 
 import stars from '../data/binary-stars';
@@ -12,22 +12,14 @@ const initialValues = {
   period: stars[0].period
 };
 
-let planner = new VariableStarPlanner();
-let scheduler = [], formDate, date;
-
 export default function planner (state = [], action) {
   switch (action.type) {
   case UPDATE_PLANNER:
-    planner.ep = action.data.ep;
-    planner.period = action.data.period;
-    formDate = action.data.year + '-' + (action.data.month-1) + '-' + action.data.day;
-    date = new Date(formDate);
+    let planner = new VariableStarPlanner(action.data.ep, action.data.period);
+    let formDate = action.data.year + '-' + (action.data.month-1) + '-' + action.data.day;
+    let date = new Date(formDate);
     return planner.getScheduler(date,  10);
   default:
-    planner.ep = initialValues.ep;
-    planner.period = initialValues.period;
-    formDate = initialValues.year + '-' + (initialValues.month-1) + '-' + initialValues.day;
-    date = new Date(formDate);
-    return planner.getScheduler(date,  10);
+    return state;
   }
 }
